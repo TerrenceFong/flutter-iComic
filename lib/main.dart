03749fa39ee9d727d8765f7de1f60b7b8101ad16
TerrenceFong <272786949@qq.com>
2021-07-13 00:23:17 +0800

    feat: add stateful iconButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -92,11 +92,7 @@ Widget titleSection = Container(
           ],
         ),
       ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      Text('41')
+      FavoriteWidget(),
     ],
   ),
 );
@@ -113,3 +109,52 @@ Widget textSection = Container(
     softWrap: true,
   ),
 );
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favariteCount = 42;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favariteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favariteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favariteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
